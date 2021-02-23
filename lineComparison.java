c2a927dfd3537a8e05e3362609801e1a5412c064
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-23 17:30:50 +0530

    Added: Calculate Length of Line Using Cartesian Co-ordinates System

--- lineComparison.java
+++ lineComparison.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
 public class lineComparison {
 
 	public static void main (String[] args) {
 
 		System.out.println("Welcome to Line Comparison Computation Problem");
 
+			int x1,y1,x2,y2;
+			Scanner input = new Scanner(System.in);
+			System.out.println("Co-ordinates for Line1");
+			System.out.println("Enter x1: ");
+			x1 = input.nextInt();
+			System.out.println("Enter y1: ");
+			y1 = input.nextInt();
+			System.out.println("Enter x2: ");
+			x2 = input.nextInt();
+			System.out.println("Enter y2: ");
+			y2 = input.nextInt();
+
+			double length1 = Math.sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
+			System.out.println("Length of Line1: "+length1);
 	}
 }
